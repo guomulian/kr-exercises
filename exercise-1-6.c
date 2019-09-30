bca32ce16411410ce5dae50780915a1061778295
Anne Kwok <guomulian@gmail.com>
2019-09-29 21:54:21 -0400

    Renamed files and added solutions for 1-5 through 1-7.

---
+++ exercise-1-6.c
@@ -0,0 +1,7 @@
+/*
+
+If getchar() has the same value as EOF, getchar() != EOF will be false, or 0
+If getchar() has a different value from EOF, getchar() != EOF will be true, or 1
+
+This is "undesired" in the copy program since c is meant to be the input character
+*/
