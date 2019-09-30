1a7c34229018a214527c1e47f7b0f1b7866ce06b
Anne Kwok <guomulian@gmail.com>
2019-09-29 23:56:06 -0400

    Added exercise 1-13.

---
+++ chapter-1/exercise-1-13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* create a histogram of the lengths of words in the input */
+
+#define MAXLEN 15
+
+int main()
+{
+    int c, l, i;
+    int lengths[MAXLEN]; // bin size will be one for convenience
+
+    for (l = 0; l < MAXLEN; ++l)
+        lengths[l] = 0;
+
+    l = 0; // reuse i
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            ++lengths[l];
+            l = 0;
+        }
+        else
+            ++l;
+    }
+
+    printf("\nHorizontal Histogram\n\n");
+    for (l = 0; l < MAXLEN; ++l) {
+        printf("%2d ", l);
+        for (i = 0; i < lengths[l]; ++i)
+            printf("#");
+        printf("\n");
+    }
+}
